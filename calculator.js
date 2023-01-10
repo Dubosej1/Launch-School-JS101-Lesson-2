f2053257ad94e839e50f2bc6f557d557bd01b77b
Johnaven Dubose <johnavenfreelance@gmail.com>
2023-01-10 13:45:41 +0000

    Create 'prompt' function to handle console.logs

--- calculator.js
+++ calculator.js
@@ -1,21 +1,25 @@
 const readline = require('readline-sync');
 
-console.log('Welcome to Calculator');
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+prompt('Welcome to Calculator');
 
 // Ask the user for the first number
 
-console.log("What's the first number?");
+prompt("What's the first number?");
 let number1 = readline.question();
 
 
 // Ask the user for the second number
 
-console.log("What's the second number?");
+prompt("What's the second number?");
 let number2 = readline.question();
 
 // Ask the user for an operation to perform.
 
-console.log("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide?");
+prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide?");
 let operation = readline.question();
 // Perform the operation on the 2 numbers.
 
