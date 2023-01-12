7eb1a981fafac9adab2501a236a1eed7de1e7d82
Johnaven Dubose <johnavenfreelance@gmail.com>
2023-01-12 13:52:57 +0000

    Refactor validate user input for loan amount

--- loan-calculator.js
+++ loan-calculator.js
@@ -4,57 +4,62 @@ function prompt (message) {
   console.log(message);
 }
 
-function validateLoanAmount(input) {
-  let errorMessage = null;
-  let parsedInput;
+function requestLoanAmount () {
+  let loanAmount;
   
-  // Check str for $ sign
-  if (input.includes('$')) {
-    if (input[0] !== '$' || charCounter(input, '$') > 1) {
-      errorMessage = `${input} should be in either 0.00 or $0.00 format.  Please try again.`;
-      return [errorMessage, input];
-    } else {
-      input = input.slice(1);
-    }
+  prompt("What is the loan amount in dollars?")
+  let userInput = READLINE.question();
+  
+  // Validate loan amount input
+  while (true) {
+  let [errorMessage, num] =  validateLoanAmount(userInput);
+  
+  if (errorMessage) {
+    prompt(errorMessage);
+    userInput = READLINE.question();
+    continue;
+  }
+  loanAmount = Number(num);
+  break;
+  }
+  return loanAmount;
+  
+  function validateLoanAmount(input) {
+  let errorMessage = null;
+  
+  // Check str for $ sign and parse out
+  if (input[0] === '$') {
+    input = input.slice(1);
   }
   
+  // Check if str is a valid number
   if (input.trimStart() === '' || Number.isNaN(Number(input))) {
     errorMessage = "Not a valid number.  Please try again.";
   }
   
   return [errorMessage, input];
 }
-
-function charCounter(string, searchChar) {
-  let count = 0;
-  
-  for (let char of string) {
-    if (char === searchChar) {
-      count += 1;
-    }
-  }
-  
-  return count;
 }
+
+
+
+// function charCounter(string, searchChar) {
+//   let count = 0;
+  
+//   for (let char of string) {
+//     if (char === searchChar) {
+//       count += 1;
+//     }
+//   }
+  
+//   return count;
+// }
 
 prompt("Welcome to the loan calculator!");
 
 while (true) {
   
   // Ask User for Loan Amount
-  prompt("What is the loan amount in dollars?")
-  let inputLoanAmount = READLINE.question();
+ let loanAmount = requestLoanAmount();
   
-  // Validate loan amount input
-  while (true) {
-  let [errorMessage, loanAmount] =  validateLoanAmount(inputLoanAmount);
-  
-  if (errorMessage) {
-    prompt(errorMessage);
-    inputLoanAmount = READLINE.question();
-    continue;
-  }
-  break;
-  }
-  console.log('success');
 }
